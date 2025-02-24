99a2d02df95f075130aa5485fb8adc2356c79475
Mark Kim <markuskimius@gmail.com>
2025-02-23 21:04:37 -0500

    cleanup

--- tests/wasm-io/test.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-
-__attribute__((export_name("shout"))) int shout(const char* text1, const char* text2) {
-    return printf("%s, %s!\n", text1, text2);
-}
-
-int main() {
-    return 0;
-}
--- tests/wasm/test.c
+++ tests/wasm/test.c
@@ -1,28 +1,13 @@
-#include "math.h"
-#include "stdint.h"
-#include "stdio.h"
-#include "stdlib.h"
-
-char* sprintb(char* str, double d) {
-    uint64_t v = *(uint64_t*)&d;
-    int i = 0;
-
-    for(i=0; i<64; i++) {
-        int bit = (v >> (63-i)) & 1;
-
-        str[i] = bit + '0';
-    }
-
-    str[i] = '\0';
-
-    return str;
-}
+#include <stdio.h>
 
 int main() {
+    double nan = 0.0 / 0.0;
     double inf = 1.0 / 0.0;
+    double ninf = -1.0 / 0.0;
 
-    printf("pow(-inf,3.141592)  -> %lf\n", pow(-inf,3.141592));
-    printf("pow(-inf,-3.141592) -> %lf\n", pow(-inf,-3.141592));
+    printf("nan = %lf, %d\n", nan, (int)nan);
+    printf("inf = %lf, %d\n", inf, (int)inf);
+    printf("ninf = %lf, %d\n", ninf, (int)ninf);
 
     return 0;
 }
